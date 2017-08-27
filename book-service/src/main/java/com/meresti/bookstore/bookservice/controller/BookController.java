fbfb0ffcdeca422f70c3f39ca5fa9acfbdc736d6
meresti <zsolt.horvat@gmail.com>
2017-08-27 18:33:40 +0300

    book service: basic CRUD operations + postman collection

--- book-service/src/main/java/com/meresti/bookstore/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/meresti/bookstore/bookservice/controller/BookController.java
@@ -4,10 +4,11 @@ import com.meresti.bookstore.bookservice.model.Book;
 import com.meresti.bookstore.bookservice.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,4 +26,44 @@ public class BookController {
     public ResponseEntity<List<Book>> getAll() {
         return ResponseEntity.ok().body(bookRepository.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getOne(@PathVariable Long id) {
+        final Book book = bookRepository.findOne(id);
+        if (book != null) {
+            return ResponseEntity.ok().body(book);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveOne(@Valid @RequestBody Book book) {
+        final Book savedBook = bookRepository.save(book);
+
+        final URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedBook.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateOne(@PathVariable Long id, @Valid @RequestBody Book book) {
+        final Book bookToUpdate = bookRepository.findOne(id);
+        if (bookToUpdate != null) {
+            book.setId(id);
+            final Book updatedBook = bookRepository.save(book);
+            return ResponseEntity.ok().body(updatedBook);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOne(@PathVariable Long id) {
+        bookRepository.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
