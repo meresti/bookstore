61fabe269b7d8590623b4a3b1e8dad87cec1990e
meresti <zsolt.horvat@gmail.com>
2017-09-07 16:22:11 +0300

    eureka service

--- book-service/src/main/java/com/meresti/bookstore/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/meresti/bookstore/bookservice/BookServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.Collections;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BookServiceApplication {
 
