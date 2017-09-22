0cb6fc763115001dc064766d0b15be9bbd92fecf
meresti <zsolt.horvat@gmail.com>
2017-09-22 21:26:21 +0300

    encrypted property values

---
+++ book-service/src/main/java/com/meresti/bookstore/bookservice/controller/HelloWorldEncryptedController.java
@@ -0,0 +1,25 @@
+package com.meresti.bookstore.bookservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HelloWorldEncryptedController {
+
+    private final String encryptedMessage;
+
+    @Autowired
+    public HelloWorldEncryptedController(@Value("${encrypted-message}") String encryptedMessage) {
+        this.encryptedMessage = encryptedMessage;
+    }
+
+    @GetMapping("/encrypted-message")
+    public String getEncryptedText() {
+        return encryptedMessage;
+    }
+
+}
