3ac89dd349fda2c0c0575638a577d82e29aad3dc
Zsolt Horvat <zsolt.horvat@accenture.com>
2020-03-28 22:01:34 +0200

    Switch to latest Spring Cloud solution

--- book-service/src/main/java/com/meresti/bookstore/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/meresti/bookstore/bookservice/controller/BookController.java
@@ -2,7 +2,8 @@ package com.meresti.bookstore.bookservice.controller;
 
 import com.meresti.bookstore.bookservice.model.Book;
 import com.meresti.bookstore.bookservice.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -12,24 +13,23 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("books")
 public class BookController {
 
     private final BookRepository bookRepository;
 
-    @Autowired
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
     @GetMapping
     public ResponseEntity<List<Book>> getAll() {
+        log.info("Retrieving all books...");
         return ResponseEntity.ok().body(bookRepository.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getOne(@PathVariable Long id) {
+        log.info("Retrieving book with identifier {}...", id);
         final Optional<Book> optionalBook = bookRepository.findById(id);
         return optionalBook
                 .map(book -> ResponseEntity.ok().body(book))
@@ -38,6 +38,7 @@ public class BookController {
 
     @PostMapping
     public ResponseEntity<?> insertOne(@Valid @RequestBody Book book) {
+        log.info("Adding new book {}...", book.getIsbn());
         final Book savedBook = bookRepository.save(book);
 
         final URI location = ServletUriComponentsBuilder
@@ -49,6 +50,7 @@ public class BookController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateOne(@PathVariable Long id, @Valid @RequestBody Book book) {
+        log.info("Updating book with identifier {}...", id);
         final Optional<Book> bookToUpdate = bookRepository.findById(id);
         if (bookToUpdate.isPresent()) {
             book.setId(id);
@@ -61,6 +63,7 @@ public class BookController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteOne(@PathVariable Long id) {
+        log.info("Deleting book with identifier {}...", id);
         bookRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
