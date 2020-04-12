c94d891fe2171b09672f7cb1239c6dca5a0eca8e
Zsolt Horvat <zsolt.horvat@accenture.com>
2020-04-12 20:07:02 +0300

    Switch to latest Spring Cloud solution

---
+++ gateway/src/main/java/com/meresti/gateway/FallbackController.java
@@ -0,0 +1,20 @@
+package com.meresti.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequestMapping("fallback")
+public class FallbackController {
+
+    @GetMapping
+    public Mono<String> fallback(final Throwable t) {
+        log.info("Error forwarding call to desired microservice.", t);
+        return Mono.just("Error forwarding call to desired microservice.");
+    }
+
+}
